af9f4a9b72d1fef5c8ad9cb6e6e736e1c0582833
root <root@DESKTOP-54PPHII.localdomain>
2024-09-20 14:27:33 +0200

    Visualization

--- conection.py
+++ conection.py
@@ -8,66 +8,54 @@
 import pandas as pd
 import numpy as np
 
-# Libaries for Mysql conection
+# Libraries for MySQL connection
 import mysql.connector
 from mysql.connector import errorcode
 
-pd.set_option('display.max_columns', None) 
+pd.set_option('display.max_columns', None)
 
 
-# We create the connection with the arguments
-cnx = mysql.connector.connect(user='root', password='AlumnaAdalab',
-                              host='127.0.0.1',
-                              database='mydb')
+# Función para manejo de errores y conexión a la base de datos
+def create_connection():
+    try:
+        cnx = mysql.connector.connect(user='root', password='AlumnaAdalab',
+                                      host='127.0.0.1',
+                                      database='abc_corp_employees')
+        return cnx
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Something is wrong with your user name or password')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print('Database does not exist')
+        else:
+            print(err)
+        return None
 
-print(cnx)
-cnx.close()
 
-# We do a try except, this allowed us to do error handling, to prevent our code from stalling. So what we're doing is
-## try to make the connection to the BBDD 
-try:
-  cnx = mysql.connector.connect(user='root', password='AlumnaAdalab',
-                              host= '127.0.0.1',
-                              database='mydb')
-# If there is some error do:
-except mysql.connector.Error as err:
-
-  # If it's an error with the password return me an access denied message as we have problems with the password.
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print('Something is wrong with your user name or password')
-  
-  # If the error is the database does not exist, return a message that the database does not exist
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print('Database does not exist')
-  
-  # If it's not for any of the above errors, print me what error I'm getting on my connection
-  else:
-    print(err)
-else:
-  cnx.close()
+# Intentar crear la conexión
+cnx = create_connection()
+if cnx:
+    print("Conexión exitosa:", cnx)
+    cnx.close()
 
 # DATA INSERTION
-# Previous to this step, we would create our database with its corresponding tables and restrictions. 
-# but, in our case, we have already created it with MySQL Workbench, so let's proceed to the insertion of the data.
+# Procedemos a la inserción de datos
 
 try:
     # Cargar datos desde el archivo CSV
     df = pd.read_csv('Files/HR RAW DATA CLEAN.csv')
 
     # Configuración de la conexión
-    cnx = mysql.connector.connect(
-        user='root',
-        password='AlumnaAdalab',
-        host='127.0.0.1',
-        database='mydb'
-    )
+    cnx = create_connection()
+    if cnx is None:
+        raise Exception("No se pudo establecer la conexión a la base de datos")
 
     cursor = cnx.cursor()
 
-    # Consulta SQL de inserción
+    # Consulta SQL de inserción (comillas invertidas en los nombres de tablas y columnas)
     query_personaldetails = """
-        INSERT INTO 'personaldetails'
-        ('EmployeeNumber', 'Age', 'Education', 'EducationField', 'Gender', 'MaritalStatus', 'DateBirth')
+        INSERT INTO `personaldetails`
+        (`EmployeeNumber`, `Age`, `Education`, `EducationField`, `Gender`, `MaritalStatus`, `DateBirth`)
         VALUES (%s, %s, %s, %s, %s, %s, %s)
     """
 
@@ -79,41 +67,45 @@ try:
         df["EducationField"].values,
         df["Gender"].values,
         df["MaritalStatus"].values,
-        df["DateBirth"].values  
+        df["DateBirth"].values
     ))
 
+    # Convertir columnas que deban ser enteros
+    def convert_int(lista_tuplas):
+        data_table_def = []
+        for tupla in lista_tuplas:
+            lista_intermedia = []
+            for elemento in tupla:
+                try:
+                    lista_intermedia.append(int(elemento))
+                except:
+                    lista_intermedia.append(elemento)
+            data_table_def.append(tuple(lista_intermedia))
+        return data_table_def
+
+    # Convertir los datos a enteros donde sea necesario
+    personal_details = convert_int(data_table_personaldetails)
+
     # Ejecutar la inserción masiva
-    cursor.executemany(query_personaldetails, data_table_personaldetails)
+    cursor.executemany(query_personaldetails, personal_details)
     cnx.commit()
 
     print("Datos insertados correctamente")
 
+    # Id con autoincremento (no es necesario después de `executemany`)
+    last_employee_number = cursor.lastrowid
+    print(f"Último ID insertado: {last_employee_number}")
+
 except mysql.connector.Error as err:
-        print(f"Error al ejecutar la consulta: {err}")
-
+    print(f"Error al ejecutar la consulta: {err}")
+except Exception as e:
+    print(f"Error general: {e}")
 finally:
-    if 'cursor' in locals():
+    if cursor:
         cursor.close()
-    if 'cnx' in locals() and cnx.is_connected():
+    if cnx and cnx.is_connected():
         cnx.close()
-        print("Conexión cerrada")
 
-def convertir_int(lista_tuplas):
-    datos_tabla_caract_def = []
-    for tupla in lista_tuplas:
-        lista_intermedia = []
-        for elemento in tupla:
-            try:
-                lista_intermedia.append(int(elemento))
-            except:
-                lista_intermedia.append(elemento)
-            
-        datos_tabla_caract_def.append(tuple(lista_intermedia))
-    
-    return datos_tabla_caract_def
-
-# Creas una función que te pase las columnas que haya que pasar a int. Crea una lista con los que ha podido convertir en int y los que no, sería algo así:
-
-
+  
 
 # %%
